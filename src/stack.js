d0a10ff46922b5c1e18a2260353da423fbce5687
Milana Kasumava <milanamamedova52@mail.ru>
2023-04-16 00:58:03 +0300

    feat:finish stack task

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (){
+    this.arr=[]
+  }
+  
+  push(elem1) {
+  this.arr.push(elem1)
+  return this
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  return this.arr.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.arr[this.arr.length-1]
   }
 }
 
